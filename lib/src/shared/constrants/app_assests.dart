e135b4698265deba1c13cc14be40cdfedd7c1fcb
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-03-01 23:15:27 +0700

    add app assets, app strings

---
+++ lib/src/shared/constrants/app_assests.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static const String youtubeBrandName = 'assets/icons/youtube.svg';
+  static const String youtubeLogo = 'assets/icons/youtube_logo.svg';
+  static const String googleLogo = 'assets/icons/google.svg';
+  static const String facebookLogo = 'assets/icons/facebook_logo.svg';
+}
---
+++ lib/src/shared/constrants/app_strings.dart
@@ -0,0 +1,8 @@
+class AppStrings {
+  static const String titleIntro = 'Welcome \nto best video app in wolrd';
+  static const String subTitleIntro =
+      'Lorem ipsum dolor sit amet, consectetur \nadipiscing elit. Lectus curabitur cursus arcu \nmollis commodo fringilla.';
+  static const String logIn = 'Log in';
+  static const String dontHaveAccount = "Don't have an account? ";
+  static const String signUp = 'Sign up';
+}
