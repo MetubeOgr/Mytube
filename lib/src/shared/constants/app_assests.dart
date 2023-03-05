7dd83a788404e33cd90ddc72ba1c6fb56b1faaec
Nguyễn Thành Tâm <nguyenthanhtam12b3@gmail.com>
2023-03-05 20:25:18 +0700

    add assets and string

--- lib/src/shared/constants/app_assests.dart
+++ lib/src/shared/constants/app_assests.dart
@@ -3,4 +3,8 @@ class AppAssets {
   static const String youtubeLogo = 'assets/icons/youtube_logo.svg';
   static const String googleLogo = 'assets/icons/google.svg';
   static const String facebookLogo = 'assets/icons/facebook_logo.svg';
+  static const String email = 'assets/icons/email.svg';
+  static const String lock = 'assets/icons/lock.svg';
+  static const String visible = 'assets/icons/eye-open.svg';
+  static const String invisible = 'assets/icons/eye-close.svg';
 }
--- lib/src/shared/constants/app_strings.dart
+++ lib/src/shared/constants/app_strings.dart
@@ -5,4 +5,7 @@ class AppStrings {
   static const String logIn = 'Log in';
   static const String dontHaveAccount = "Don't have an account? ";
   static const String signUp = 'Sign up';
+  static const String email = 'Email';
+  static const String password = 'Password';
+  static const String forgetPassword = 'Forget password ?';
 }
