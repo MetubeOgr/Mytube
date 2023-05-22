e7289ee09f57b545f3912e88c1eb318bd7f0db55
Nguyễn Thành Tâm <71802989+tamdev3241@users.noreply.github.com>
2023-05-22 21:55:59 +0700

    add list (#4)

--- lib/src/screens/home/widgets/channel_followed_list.dart
+++ lib/src/screens/home/widgets/channel_followed_list.dart
@@ -9,33 +9,36 @@ class ChannelFollowedList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double avatarImgSize = 0.0;
-    EdgeInsetsGeometry avatarImgPadding = EdgeInsets.zero;
+    double _avatarImgSize = 0.0;
+    double _followsListHeight = 0.0;
+    EdgeInsetsGeometry _avatarImgPadding = EdgeInsets.zero;
     switch (DeviceConfig.deviceScreenType) {
       case DeviceScreenType.mobile:
-        avatarImgSize = 23.0;
-        avatarImgPadding = const EdgeInsetsDirectional.symmetric(
+        _avatarImgSize = 23.0;
+        _avatarImgPadding = const EdgeInsetsDirectional.symmetric(
           horizontal: 5,
           vertical: 15,
         );
+        _followsListHeight = 80;
         break;
       case DeviceScreenType.desktop:
       case DeviceScreenType.tablet:
-        avatarImgSize = 30.0;
-        avatarImgPadding = const EdgeInsetsDirectional.symmetric(
+        _avatarImgSize = 30.0;
+        _avatarImgPadding = const EdgeInsetsDirectional.symmetric(
           horizontal: 10,
           vertical: 20,
         );
+        _followsListHeight = 100;
         break;
     }
-    return Flexible(
-      flex: 1,
+    return SizedBox(
+      height: _followsListHeight,
       child: ListView.builder(
         itemCount: 15,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) => AppCircleAvatar(
-          padding: avatarImgPadding,
-          size: avatarImgSize,
+          padding: _avatarImgPadding,
+          size: _avatarImgSize,
           backgroundImage: AssetImage(AppAssets.avatar_1),
           isActive: index % 2 == 0,
           onTapped: () {
